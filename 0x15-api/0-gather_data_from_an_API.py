458b5b5c3b3a071bd3648c5695ec704e31e3aedb
khaldi505 <habetlbrika69@gmail.com>
2020-07-16 19:22:39 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+script that, using this REST API, for a given employee ID,
+returns information about his/her
+TODO list progress.
+"""
 import requests
 import sys
 tasks = 0
