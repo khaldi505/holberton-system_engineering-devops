04a2c88dbe22bd34eace7b4fd8b1c679c90cf7fc
khaldi505 <habetlbrika69@gmail.com>
2020-07-17 20:39:16 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,25 +6,31 @@ TODO list progress.
 """
 import requests
 import sys
-tasks = 0
-done = 0
-titles = ""
-username = ""
-users = requests.get("https://jsonplaceholder.typicode.com/users")
-users = users.json()
-for x in range(len(users)):
-    if(int(sys.argv[1]) == users[x]["id"]):
-        username = users[x]["name"]
-        break
-users = requests.get("https://jsonplaceholder.typicode.com/todos")
-users = users.json()
-for y in range(len(users)):
-    if (int(sys.argv[1]) == users[y]["userId"]):
-        tasks += 1
-        if (users[y]["completed"]):
-            done += 1
-            titles += "\t" + " " + users[y]["title"] + '\n'
 
-parg = "Employee {} is done with tasks({}/{})".format(username, done, tasks)
-print(parg)
-print("{}".format(titles), end='')
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit()
+    tasks = 0
+    done = 0
+    titles = ""
+    username = ""
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1]))
+    if users.status_code == 200:
+        users = users.json()
+        username = users["name"]
+        users = requests.get("https://jsonplaceholder.typicode.com/todos")
+        if users.status_code != 200:
+            exit()
+        users = users.json()
+        for y in range(len(users)):
+            if (int(sys.argv[1]) == users[y]["userId"]):
+                tasks += 1
+                if (users[y]["completed"]):
+                    done += 1
+                    titles += "\t" + " " + users[y]["title"] + '\n'
+
+        parg = "Employee {} is done with tasks({}/{})".format(
+            username, done, tasks)
+        print(parg)
+        print("{}".format(titles), end='')
