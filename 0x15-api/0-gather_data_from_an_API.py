a47b8e3affe2b552cbfadac6f76657b7c68e821d
khaldi505 <53900877+khaldi505@users.noreply.github.com>
2020-07-17 16:17:33 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,21 +14,22 @@ if __name__ == "__main__":
     titles = ""
     username = ""
     users = requests.get("https://jsonplaceholder.typicode.com/users")
-    users = users.json()
-    for x in range(len(users)):
-        if(int(sys.argv[1]) == users[x]["id"]):
-            username = users[x]["name"]
-            break
-    users = requests.get("https://jsonplaceholder.typicode.com/todos")
-    users = users.json()
-    for y in range(len(users)):
-        if (int(sys.argv[1]) == users[y]["userId"]):
-            tasks += 1
-            if (users[y]["completed"]):
-                done += 1
-                titles += "\t" + " " + users[y]["title"] + '\n'
+    if users.status_code == 200:
+        users = users.json()
+        for x in range(len(users)):
+            if(int(sys.argv[1]) == users[x]["id"]):
+                username = users[x]["name"]
+                break
+        users = requests.get("https://jsonplaceholder.typicode.com/todos")
+        users = users.json()
+        for y in range(len(users)):
+            if (int(sys.argv[1]) == users[y]["userId"]):
+                tasks += 1
+                if (users[y]["completed"]):
+                    done += 1
+                    titles += "\t" + " " + users[y]["title"] + '\n'
 
-    parg = "Employee {} is done with tasks({}/{})".format(
-        username, done, tasks)
-    print(parg)
-    print("{}".format(titles), end='')
+        parg = "Employee {} is done with tasks({}/{})".format(
+            username, done, tasks)
+        print(parg)
+        print("{}".format(titles), end='')
