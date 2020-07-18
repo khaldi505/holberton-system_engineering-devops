7840c591a36aaf18c3c4f910366ba51aabe1d503
khaldi505 <habetlbrika69@gmail.com>
2020-07-18 12:33:18 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,26 +7,24 @@ TODO list progress.
 if __name__ == "__main__":
     import requests
     from sys import argv
-    u_id = argv[1]
+    u_id = int(argv[1])
     user_name = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
+        "https://jsonplaceholder.typicode.com/users?id={}".format(u_id)
         ).json()
-    user_name = user_name["name"]
+    user_name = user_name[0]["name"]
     todos = requests.get(
         "https://jsonplaceholder.typicode.com/todos/?userId={}".format(u_id)
         ).json()
     x = 0
     todo_titles = []
     done = 0
-    tasks = 0
     for todo in todos:
-        tasks += 1
         if todo["completed"]:
             done += 1
             todo_titles.append(todo["title"])
     print(
         "Employee {:s} is done with tasks({:d}/{:d}):"
-        .format(user_name, done, tasks)
+        .format(user_name, done, len(todos))
         )
     for x in todo_titles:
         print("\t {:s}".format(x))
