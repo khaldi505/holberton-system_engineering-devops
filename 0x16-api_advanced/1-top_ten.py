f183ed5448279624970ee4820540f2e10f8ff014
khaldi505 <habetlbrika69@gmail.com>
2020-09-17 17:04:48 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,11 @@ def top_ten(subreddit):
     link = "https://www.reddit.com/r/{}/top.json?limit=10".format(subreddit)
     result = requests.get(link, headers={"User-Agent": "Mozilla/5.0"})
     result = result.json()
+    if result["data"]["children"] == []:
+        print("None")
     try:
         result = result["data"]["children"]
         for x in result:
-            print(x["data"]["title"])
+            print("{}".format(x["data"]["title"]))
     except Exception as error:
         print("None")
