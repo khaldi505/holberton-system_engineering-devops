6f528bd1f1544a93df3519ee38468b9d0db84f10
khaldi505 <habetlbrika69@gmail.com>
2020-09-17 16:57:17 +0100

    argv removed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,8 +5,7 @@
 
 def top_ten(subreddit):
     import requests
-    from sys import argv
-    link = "https://www.reddit.com/r/{}/top.json?limit=10".format(argv[1])
+    link = "https://www.reddit.com/r/{}/top.json?limit=10".format(subreddit)
     result = requests.get(link, headers={"User-Agent": "Mozilla/5.0"})
     result = result.json()
     if result["data"]["children"] == []:
