0b122f733bef4cfa3b2c1a6d5c4dff4f66ab205b
khaldi505 <habetlbrika69@gmail.com>
2020-09-30 16:25:46 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fixes type of php files in a config file
-exec { ‘replaces line in config file’: command => ‘sed -i “s/.phpp/.php/g” /var/www/html/wp-settings.php’,
-path => ‘/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin’,
-onlyif => ‘test -f /var/www/html/wp-settings.php’
+exec { 'replaces line in config file': command => 'sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php',
+path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+onlyif => 'test -f /var/www/html/wp-settings.php'
 }
