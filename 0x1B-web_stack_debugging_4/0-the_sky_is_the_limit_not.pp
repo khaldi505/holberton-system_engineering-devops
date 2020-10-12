b9acb970bab5b4a7f39b6339f92e1d4d6d152365
khaldi505 <53900877+khaldi505@users.noreply.github.com>
2020-10-12 22:41:26 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # changing the ULIMIT value
 
-exec{'ulimit_fix':
+exec {'ulimit_fix':
 path    => [ '/usr/local/bin/', '/bin/' ],
 command => 'sudo sed -i 's/-n 15/-n 2000/g' /etc/default/nginx; sudo service nginx restart',
 }
